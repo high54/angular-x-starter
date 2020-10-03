a4636356fd9760e9ddc327778549baa0e5373299
high54 <julien.bertacco@gmail.com>
2020-10-03 13:04:10 +0200

    Ajout du composant checkbox de Material

--- src/app/core/authentication/authentication-material.module.ts
+++ src/app/core/authentication/authentication-material.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
     imports: [
         MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule,
-        MatCardModule
+        MatInputModule
     ],
     exports: [
         MatButtonModule,
+        MatCardModule,
+        MatCheckboxModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule,
-        MatCardModule
+        MatInputModule
     ]
 })
 export class AuthenticationMaterialModule { }
