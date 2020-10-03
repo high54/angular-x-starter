997e8523b72235b4afeae0c3c624c332a85f174a
high54 <julien.bertacco@gmail.com>
2020-10-03 09:58:40 +0200

    Ajout des composants Material pour la module d'Authentification

--- src/app/core/authentication/authentication-material.module.ts
+++ src/app/core/authentication/authentication-material.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatCardModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatCardModule
+    ]
 })
 export class AuthenticationMaterialModule { }
