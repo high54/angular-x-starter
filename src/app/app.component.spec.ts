c612133d02909a5d7d5ca2afa0cb6f6cbd4839e3
high54 <julien.bertacco@gmail.com>
2020-10-12 19:42:16 +0200

    Ajout de nouveau tests unitaires pour le composant APP

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,52 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SwUpdate, UpdateAvailableEvent, UpdateActivatedEvent } from '@angular/service-worker';
+// Components
 import { AppComponent } from './app.component';
+import * as fromUIComponents from './core/ui/components';
+// Mocks
+import { materialModules } from './mocks/material-modules.mock';
+// Rxjs
+import { EMPTY, of } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
+export class SwUpdateServerMock {
+  public available: Observable<UpdateAvailableEvent> = of(({} as UpdateAvailableEvent));
+  public activated: Observable<UpdateActivatedEvent> = new Subject();
+  public isEnabled = true;
+
+  public checkForUpdate(): Promise<void> {
+    return new Promise((resolve) => resolve());
+  }
+  public activateUpdate(): Promise<void> {
+    return new Promise((resolve) => resolve());
+  }
+}
+const mockSwUpdate = {
+  isEnabled: true,
+  activated: EMPTY,
+  available: EMPTY,
+  checkForUpdate: () => of(null).toPromise<void>(),
+  activateUpdate: () => of(null).toPromise<void>()
+};
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        ...materialModules
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ...fromUIComponents.components
       ],
+      providers: [
+        { provide: SwUpdate, useClass: SwUpdateServerMock }
+      ]
     }).compileComponents();
   }));
 
@@ -23,13 +59,22 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-x-starter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-x-starter');
+    expect(app.title).toEqual('Angular X Starter');
   });
 
-  it('should render title', () => {
+  it(`Trigger OnInit`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-x-starter app is running!');
+    const app = fixture.componentInstance;
+    spyOn(window, 'confirm').and.returnValue(false);
+    expect(app.ngOnInit()).toBeTruthy();
+  });
+  it('changeTheme to darkmode', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const darkMode = false;
+    localStorage.setItem('darkMode', darkMode.toString());
+    app.darkMode.setValue(true);
+    app.changeTheme();
+    expect((localStorage.getItem('darkMode') === 'true')).toBeTrue();
   });
 });
