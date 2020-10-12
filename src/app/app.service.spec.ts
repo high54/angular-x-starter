eb8816f8e4f636c3ee6dbceab9c861d3bfcc11c1
high54 <julien.bertacco@gmail.com>
2020-10-12 19:42:02 +0200

    Modification des tests unitaires pour le service APP

--- src/app/app.service.spec.ts
+++ src/app/app.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppService } from './app.service';
+import { Meta, Title } from '@angular/platform-browser';
+import { EMPTY, of } from 'rxjs';
 
 describe('AppService', () => {
   let service: AppService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Meta, useValue: { addTag: (params?) => { }, removeTag: (params?) => { }, addTags: (params?) => { } } },
+        { provide: Title, useValue: { setTitle: (params?) => { } } }
+      ]
+    });
     service = TestBed.inject(AppService);
   });
 
