74068ee5b2c68337cff4239815c0c5b8525c4829
high54 <julien.bertacco@gmail.com>
2020-10-14 19:41:52 +0200

    Ajout des modules matérial manquant pour les tests

--- src/app/mocks/material-modules.mock.ts
+++ src/app/mocks/material-modules.mock.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -19,6 +21,8 @@ export const materialModules: any[] = [
     MatCardModule,
     MatCheckboxModule,
     MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
