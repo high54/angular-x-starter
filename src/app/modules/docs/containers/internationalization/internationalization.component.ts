1e3674da88a52a5101ba3245f200a8b8b0fbf942
high54 <julien.bertacco@gmail.com>
2020-10-19 18:13:19 +0200

    Ajout du composant pour la documentation sur l'internationalisation

---
+++ src/app/modules/docs/containers/internationalization/internationalization.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-internationalization',
+  templateUrl: './internationalization.component.html'
+})
+export class InternationalizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
