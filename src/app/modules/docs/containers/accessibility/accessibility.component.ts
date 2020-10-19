1b604cb2b7d517539a64150eb248793e652fd890
high54 <julien.bertacco@gmail.com>
2020-10-19 18:13:02 +0200

    Ajout du composant pour la documentation sur l'accessibilité

---
+++ src/app/modules/docs/containers/accessibility/accessibility.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accessibility',
+  templateUrl: './accessibility.component.html'
+})
+export class AccessibilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
