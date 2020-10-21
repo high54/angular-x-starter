8c67bca99c0d2c1e191c29caabcf7b4e4451a47d
high54 <julien.bertacco@gmail.com>
2020-10-21 17:58:32 +0200

    Correction du test sur le changement de theme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async} from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -114,7 +114,6 @@ describe('AppComponent', () => {
     const darkMode = false;
     localStorage.setItem('darkMode', darkMode.toString());
     app.darkMode = darkMode;
-    app.changeTheme();
     expect((localStorage.getItem('darkMode') === 'false')).toBeTrue();
   });
 
